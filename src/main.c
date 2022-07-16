7dcde196bd6d87e20bac82decb68de3b38042c44
linuxu <linuxu@ParallelCv4-9.academic.management.afeka.local>
2022-07-16 18:19:54 +0300

    working on MPI implementation

--- src/main.c
+++ src/main.c
@@ -72,51 +72,114 @@ void parseFile(char* path, int* matchingValue, int* numOfPics, int*** pictures, 
 	}
 }
 
-int main(int argc, char* argv[]){
-//	int  my_rank; /* rank of process */
-//	int  p;       /* number of processes */
-//	int source;   /* rank of sender */
-//	int dest;     /* rank of receiver */
-//	int tag=0;    /* tag for messages */
-//	char message[100];        /* storage for message */
-//	MPI_Status status ;   /* return status for receive */
-//
-//	/* start up MPI */
-//
-//	MPI_Init(&argc, &argv);
-//
-//	/* find out process rank */
-//	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-//
-//	/* find out number of processes */
-//	MPI_Comm_size(MPI_COMM_WORLD, &p);
-//
-//
-//	if (my_rank !=0){
-//		/* create message */
-//		sprintf(message, "Hello MPI World from process %d!", my_rank);
-//		dest = 0;
-//		/* use strlen+1 so that '\0' get transmitted */
-//		MPI_Send(message, strlen(message)+1, MPI_CHAR,
-//		   dest, tag, MPI_COMM_WORLD);
-//	}
-//	else{
-//		printf("Hello MPI World From process 0: Num processes: %d\n",p);
-//		for (source = 1; source < p; source++) {
-//			MPI_Recv(message, 100, MPI_CHAR, source, tag,
-//			      MPI_COMM_WORLD, &status);
-//			printf("%s\n",message);
-//		}
-//	}
-//	/* shut down MPI */
-//	MPI_Finalize();
-//
-//
+void runMaster(int p, char* path, int ***myPictures)
+{
 	int matching;
 	int numOfPics;
 	int** pictures;
 	int numOfObjs;
 	int** objects;
-	parseFile("/home/linuxu/ParallelComputationFinalProject/src/input2.txt", &matching, &numOfPics, &pictures, &numOfObjs, &objects);
+
+	parseFile(path, &matching, &numOfPics, &pictures, &numOfObjs, &objects);
+
+
+	if (p > numOfPics)
+	{
+		p = numOfPics;
+	}
+
+	int portionSize = numOfPics / p;
+
+	*myPictures = (int**)malloc(portionSize * sizeof(int*));
+	for(int i = 0; i < p; i++)
+	{
+		if(i==0){
+			for(int j = 0; j < portionSize; j++)
+			{
+				(*myPictures)[j] = pictures[j];
+			}
+		}else{
+			MPI_Send(&matching, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			printf("send matcing = %d\n", matching);
+			MPI_Send(&numOfObjs, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			printf("send numOfObj = %d\n", numOfObjs);
+			MPI_Send(objects, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			printf("send objects\n");
+			MPI_Send(&portionSize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			printf("send portionSize\n");
+			for(int j = i * portionSize; j < i * (portionSize + 1); j++)
+			{
+				MPI_Send(pictures[j],1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			}
+			printf("send pictures\n");
+		}
+	}
+}
+
+void runSlave()
+{
+	int matching;
+	int numOfObjs;
+	int** objects;
+	int portionSize;
+	int** pictures;
+
+	MPI_Recv(&matching, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("matching from send/recv = %d\n", matching);
+	MPI_Recv(&numOfObjs, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("numOfObjs from send/recv = %d\n", numOfObjs);
+
+	// TODO: implement malloc for objects array and each object in the array
+
+	MPI_Recv(&objects, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("Objects array from send/recv start:\n");
+	for(int i = 0; i < numOfObjs; i++)
+	{
+		printf("%d\n", *objects[i]);
+	}
+	printf("Objects array from send/recv end\n");
+	MPI_Recv(&portionSize, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("portionSize from send/recv = %d\n", portionSize);
+	printf("pictures array from send/recv start:\n");
+	for(int i = 0; i < portionSize; i++)
+	{
+		MPI_Recv(pictures[i], 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("%d\n", pictures[i]);
+	}
+	printf("pictures array from send/recv end\n");
+}
+
+int main(int argc, char* argv[]){
+	int  my_rank; /* rank of process */
+	int  p;       /* number of processes */
+	int source;   /* rank of sender */
+	int dest;     /* rank of receiver */
+	int tag=0;    /* tag for messages */
+	char message[100];        /* storage for message */
+	MPI_Status status ;   /* return status for receive */
+
+	/* start up MPI */
+
+	MPI_Init(&argc, &argv);
+
+	/* find out process rank */
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	/* find out number of processes */
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+
+	if (my_rank !=0){
+		runSlave();
+	}
+	else{
+		int** myPictures;
+		runMaster(p, "/home/linuxu/ParallelComputationFinalProject/src/input2.txt", &myPictures);
+	}
+	/* shut down MPI */
+	MPI_Finalize();
+
+
+	
 	return 0;
 }
--- src/myProto.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define PART  25000
-#define HEAVY  10000
-
-void test(int *data, int n, int resultTotal);
-int computeOnGPU(int *data, int n, int *result);
